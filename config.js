b4b0eb5528dd968156d41f4d7ad5064170b92f51
Unswear <74107088+Unswear@users.noreply.github.com>
2022-06-23 03:50:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -42,20 +42,20 @@ global.APIKeys = { // APIKey nya disini
 
 // Sticker WM
 global.packname = 'Exc'
-global.author = 'NyyXz'
+global.author = 'NyyXz'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© R-Txzy' //change the watermark 
-global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
+global.watermark = 'GroupStats //change the watermark 
+global.image = '' //change the image
+global.thumbfoto = 'https://telegra.ph/file/f14cfd3507b6055f0cf4b.jpg'
 
 // image
-global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
-global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
-global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
-global.thanks = 'https://telegra.ph/file/01917f7782b70de8f418d.jpg'
+global.bank = 'https://telegra.ph/file/f14cfd3507b6055f0cf4b.jpg'
+global.kandang = 'https://telegra.ph/file/f14cfd3507b6055f0cf4b.jpg'
+global.kolam = 'https://telegra.ph/file/f14cfd3507b6055f0cf4b.jpg'
+global.thanks = 'https://telegra.ph/file/f14cfd3507b6055f0cf4b.jpg'
 
 global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
 
